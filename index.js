08ab6a0ab2b5391d1842321cfeacd77bb85c8dc1
Kaya Jackson <kayajackson27@gmail.com>
2021-08-11 18:06:18 -0400

    todo list practice

---
+++ index.js
@@ -0,0 +1,44 @@
+var button = document.getElementById("enter");
+var input = document.querySelector("input");
+var deleteButton = document.getElementsByClassName("delete");
+
+function createElement() {
+        var li = document.createElement("li");
+        var makeButton = document.createElement("button");
+        var ul = document.querySelector("ul")
+
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+
+        makeButton.className = "delete";
+        makeButton.textContent = "delete";
+        ul.appendChild(makeButton);
+}
+
+
+function lengthCheck() {
+    if(input.value.length > 0) {
+        createElement();
+    }
+}
+
+function keyBoardCheck(e) {
+    if(input.value.length > 0 && event.keyCode === 13) {
+        createElement()
+    }
+}
+
+button.addEventListener("click", lengthCheck);
+
+input.addEventListener("keypress", keyBoardCheck);
+
+deleteButton.addEventListener("click", (e) => {
+    if (e.target.className === "delete"){
+		e.target.parentElement.remove();
+	}
+
+});
+
+
+
